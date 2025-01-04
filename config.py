61824f6c9c470eb888cc6c6476eb93dda749938e
fileuplaoderbot <tparveen2728@gmail.com>
2025-01-04 16:17:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "26683574"))
+    API_HASH = environ.get("API_HASH", "69ba051f43cff367bf569bd54eb277a7")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6069621485"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
